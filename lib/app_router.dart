030bc6827a8fa1c8609265854d1fe0bbbe4dc11b
cherrie-k <cherriekim925@gmail.com>
2024-07-03 17:17:17 +0900

    [Refactor] 라우팅 파일 분리

---
+++ lib/app_router.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nearhere/features/board/views/board_page.dart';
+import 'package:nearhere/features/home/views/home_page.dart';
+import 'package:nearhere/features/post/views/post_page.dart';
+import 'package:nearhere/shared/widgets/custom_nav_bar.dart';
+
+class AppRouter {
+  final GoRouter router = GoRouter(
+    routes: [
+      ShellRoute(
+        builder: (context, state, child) {
+          return Scaffold(
+            body: child,
+            bottomNavigationBar: CustomNavBar(
+              selectedIdx: calculateSelectedIdx(state.uri.path),
+            ),
+          );
+        },
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) => const HomePage(),
+          ),
+          GoRoute(
+            path: '/board',
+            builder: (context, state) => const BoardPage(),
+          ),
+          GoRoute(
+            path: '/post',
+            builder: (context, state) => const PostPage(),
+          ),
+        ],
+      ),
+    ],
+  );
+
+  static int calculateSelectedIdx(String location) {
+    switch (location) {
+      case '/':
+        return 0;
+      case '/post':
+        return 1;
+      case '/board':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:nearhere/features/board/views/board_page.dart';
-import 'package:nearhere/features/home/views/home_page.dart';
-import 'package:nearhere/features/post/views/post_page.dart';
-import 'package:nearhere/shared/widgets/custom_nav_bar.dart';
+import 'package:nearhere/app_router.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,56 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  final GoRouter _router = GoRouter(
-    routes: [
-      ShellRoute(
-        builder: (context, state, child) {
-          return Scaffold(
-            body: child,
-            bottomNavigationBar: CustomNavBar(
-                selectedIdx: calculateSelectedIdx(state.uri.path)),
-          );
-        },
-        routes: [
-          GoRoute(
-            path: '/',
-            builder: (context, state) => const HomePage(),
-          ),
-          GoRoute(
-            path: '/board',
-            builder: (context, state) => const BoardPage(),
-          ),
-          GoRoute(
-            path: '/post',
-            builder: (context, state) => const PostPage(),
-          ),
-        ],
-      )
-    ],
-  );
+  final AppRouter _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'NEARHERE',
-      routerConfig: _router,
+      routerConfig: _appRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // primarySwatch: Colors.blue,
       ),
     );
-  }
-}
-
-int calculateSelectedIdx(String location) {
-  switch (location) {
-    case '/':
-      return 0;
-    case '/post':
-      return 1;
-    case '/board':
-      return 2;
-    default:
-      return 0;
   }
 }
