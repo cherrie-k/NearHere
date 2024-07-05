d18942cae590c07132f63aa994c7eb86a3b39692
cherrie-k <cherriekim925@gmail.com>
2024-07-05 15:30:19 +0900

    [Feat] 카테고리 모델 enum값과 함수 추가

--- lib/shared/models/category.dart
+++ lib/shared/models/category.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+enum CategoryKey {
+  WARN,
+  FOOD,
+  FUN,
+  CUTE,
+  ETC,
+}
+
 class Category {
   final String text;
   final String iconPath;
@@ -14,35 +22,39 @@ class Category {
   });
 }
 
-const Map<String, Category> categories = {
-  'WARN': Category(
+const Map<CategoryKey, Category> categories = {
+  CategoryKey.WARN: Category(
     text: '위험',
     iconPath: 'assets/icons/warn_icon.svg',
     iconPathColored: 'assets/icons/warn_icon_colored.svg',
     color: Color(0xFFFFC9C9),
   ),
-  'FOOD': Category(
+  CategoryKey.FOOD: Category(
     text: '먹거리',
     iconPath: 'assets/icons/food_icon.svg',
     iconPathColored: 'assets/icons/food_icon_colored.svg',
     color: Color(0xFFDDFFC7),
   ),
-  'FUN': Category(
+  CategoryKey.FUN: Category(
     text: '볼거리',
     iconPath: 'assets/icons/fun_icon.svg',
     iconPathColored: 'assets/icons/fun_icon_colored.svg',
     color: Color(0xFFD7EEFF),
   ),
-  'CUTE': Category(
+  CategoryKey.CUTE: Category(
     text: '귀여움',
     iconPath: 'assets/icons/cute_icon.svg',
     iconPathColored: 'assets/icons/cute_icon_colored.svg',
     color: Color(0xFFFFF5C2),
   ),
-  'ETC': Category(
+  CategoryKey.ETC: Category(
     text: '기타',
     iconPath: 'assets/icons/etc_icon.svg',
     iconPathColored: 'assets/icons/etc_icon_colored.svg',
     color: Color(0xFFEDEDED),
   ),
 };
+
+Category? getCategory(CategoryKey key) {
+  return categories[key];
+}
