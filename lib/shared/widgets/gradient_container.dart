ad742f1e3b2e34bfede9ed8072211d4b78f97f43
cherrie-k <cherriekim925@gmail.com>
2024-07-05 13:48:42 +0900

    [Feat] GradientContainer 속성 수정

--- lib/shared/widgets/gradient_container.dart
+++ lib/shared/widgets/gradient_container.dart
@@ -3,16 +3,29 @@ import 'package:nearhere/shared/widgets/refresh_button.dart';
 
 class GradientContainer extends StatelessWidget {
   final Widget child;
-  final bool? showRefreshBtn;
+  final bool showRefreshBtn;
+  final Border? borderStyle;
+  final BorderRadius? borderRadius;
+  // final List<BoxShadow>? boxShadow;
 
-  const GradientContainer(
-      {super.key, required this.child, this.showRefreshBtn = true});
+  const GradientContainer({
+    super.key,
+    required this.child,
+    this.showRefreshBtn = true,
+    this.borderStyle,
+    this.borderRadius,
+    // this.boxShadow,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: RadialGradient(
+      decoration: BoxDecoration(
+        border: borderStyle,
+        borderRadius: borderRadius,
+        // boxShadow: boxShadow,
+        color: Colors.white,
+        gradient: const RadialGradient(
           colors: [
             Color(0xB35793CA), // #5793CA with 70% opacity
             Color(0xB360A8B4), // #60A8B4 with 70% opacity
@@ -25,11 +38,12 @@ class GradientContainer extends StatelessWidget {
       child: Stack(
         children: [
           child,
-          Positioned(
-            bottom: 16,
-            right: 16,
-            child: showRefreshBtn! ? RefreshButton() : SizedBox.shrink(),
-          ),
+          if (showRefreshBtn)
+            const Positioned(
+              bottom: 16,
+              right: 16,
+              child: RefreshButton(),
+            ),
         ],
       ),
     );
