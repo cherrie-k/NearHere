7edbd8bcd780b651e94cfab662af3a1e44381f1b
cherrie-k <cherriekim925@gmail.com>
2024-07-09 20:09:24 +0900

    [Design] 메인페이지 디자인 적용

--- lib/features/home/views/home_page.dart
+++ lib/features/home/views/home_page.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nearhere/features/home/widgets/header_area.dart';
+import 'package:nearhere/features/home/widgets/map_area.dart';
+import 'package:nearhere/features/home/widgets/target_button.dart';
 import 'package:nearhere/shared/viewmodels/location_viewmodel.dart';
 import 'package:nearhere/shared/widgets/gradient_container.dart';
 
 class HomePage extends ConsumerWidget {
-  // final String location;
-
-  const HomePage({
-    super.key,
-    //required this.location
-  });
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -26,59 +22,11 @@ class HomePage extends ConsumerWidget {
                 return Column(
                   children: <Widget>[
                     GradientContainer(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const SizedBox(height: 46),
-                          const Text(
-                            'NERE HERE!',
-                            style: TextStyle(
-                              fontFamily: 'Bangers',
-                              fontSize: 52,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text(
-                                '"',
-                                style: TextStyle(
-                                  fontFamily: 'Bangers',
-                                  fontSize: 25,
-                                ),
-                              ),
-                              Text(location.adminAddress),
-                              const Text(
-                                '"',
-                                style: TextStyle(
-                                  fontFamily: 'Bangers',
-                                  fontSize: 25,
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 46),
-                        ],
-                      ),
+                      child: HeaderArea(location: location),
                     ),
-                    const SizedBox(
-                      height: 16,
-                    ),
+                    const SizedBox(height: 16),
                     Expanded(
-                      child: NaverMap(
-                        options: NaverMapViewOptions(
-                            initialCameraPosition: NCameraPosition(
-                          target:
-                              NLatLng(location.latitude, location.longitude),
-                          zoom: 15,
-                          bearing: 0,
-                          tilt: 0,
-                        )),
-                        onMapReady: (controller) {
-                          debugPrint('네이버 맵 로딩 완료');
-                        },
-                      ),
+                      child: MapArea(location: location),
                     ),
                   ],
                 );
@@ -90,32 +38,10 @@ class HomePage extends ConsumerWidget {
                 child: Text('Error: $error'),
               ),
             ),
-            Positioned(
+            const Positioned(
               right: 20,
               bottom: 20,
-              child: Container(
-                width: 52,
-                height: 52,
-                decoration: BoxDecoration(
-                  color: const Color(0xFFF5F5F5),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.2),
-                      offset: Offset(0, 2),
-                      blurRadius: 1.2,
-                      spreadRadius: 1.2,
-                    ),
-                  ],
-                  borderRadius: BorderRadius.circular(19),
-                ),
-                child: Center(
-                  child: SvgPicture.asset(
-                    'assets/icons/target.svg',
-                    width: 32,
-                    height: 32,
-                  ),
-                ),
-              ),
+              child: TargetButton(),
             ),
           ],
         ),
